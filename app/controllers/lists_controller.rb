9c1ef9a613cc8bb67cb5a96e1682c8f7d25271d5
Chris Clifford and Jonathan Thom <chris+jonathan@>
2017-02-06 16:28:30 -0800

    Add list of complete tasks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @completeTasks = Task.completeTasks(@list.id)
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :presence => true
+
+  def self.completeTasks(list_id)
+    Task.where(:done => true, :list_id => list_id)
+  end
 end
